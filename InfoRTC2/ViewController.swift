b65933812b166131a2949ab734a7b03d6cf62aae
half-but <qudcks9191@naver.com>
2018-01-09 16:03:03 +0900

    Initial Commit

---
+++ InfoRTC2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfoRTC2
+//
+//  Created by 이병찬 on 2018. 1. 9..
+//  Copyright © 2018년 root. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
